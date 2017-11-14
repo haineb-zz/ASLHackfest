8ccdbd73fee340fb29a0b6c3830d2ff6d360ff7c
Brandon Haines <haineb@gmail.com>
2017-11-14 15:02:17 -0500

    Whitespace fixup.

--- protocol.py
+++ protocol.py
@@ -2,41 +2,41 @@ from bitstring import BitArray
 import struct
 
 class Protocol(object):
-	FALSE = '0b0'
-	TRUE = '0b1'
-	ZERO2 = '0b00'
-	ONE2 = '0b01'
-	TWO2 = '0b10'
-	THREE2 = '0b11'
+        FALSE = '0b0'
+        TRUE = '0b1'
+        ZERO2 = '0b00'
+        ONE2 = '0b01'
+        TWO2 = '0b10'
+        THREE2 = '0b11'
 
-	def __init__(self, *args, **kwargs):
-		self.version = kwargs.get('version')
+        def __init__(self, *args, **kwargs):
+                self.version = kwargs.get('version')
 
-	def __str__(self):
-		return ""
+        def __str__(self):
+                return ""
 
-	# TODO Do we need negatives? If so, this needs more testing.
-	def bin_formater(self, value, length):
-		if value is None:
-			return ("0b" + bin(0).lstrip('-0b').zfill(length))
-		else:
-			return ("0b" + bin(value).lstrip('-0b').zfill(length))
+        # TODO Do we need negatives? If so, this needs more testing.
+        def bin_formater(self, value, length):
+                if value is None:
+                        return ("0b" + bin(0).lstrip('-0b').zfill(length))
+                else:
+                        return ("0b" + bin(value).lstrip('-0b').zfill(length))
 
-	def bin_to_uint(self, value):
-		return BitArray(bin=value).uint
+        def bin_to_uint(self, value):
+                return BitArray(bin=value).uint
 
-	def bin_to_bool(self, value):
-		return BitArray(bin=value).bool
+        def bin_to_bool(self, value):
+                return BitArray(bin=value).bool
 
 
-	# Requires protocol to implement from_bitarray()
-	def from_bytearray(self, byte_array):
-		bit_array = BitArray(bytes=byte_array)
-		self.from_bitarray(bit_array)
+        # Requires protocol to implement from_bitarray()
+        def from_bytearray(self, byte_array):
+                bit_array = BitArray(bytes=byte_array)
+                self.from_bitarray(bit_array)
 
-	# Requires protocol to implement to_bitarray()
-	def to_bytearray(self):
-		return self.to_bitarray().bytes
+        # Requires protocol to implement to_bitarray()
+        def to_bytearray(self):
+                return self.to_bitarray().bytes
 
 
         '''Overload with method to calculate header content and self-update as necessary.'''
