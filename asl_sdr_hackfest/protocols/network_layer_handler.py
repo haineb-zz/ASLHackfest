3140de34cebb674c8934cb9979097580f45c73be
Brandon Haines <haineb@gmail.com>
2017-11-16 16:12:21 -0500

    Adding fragment culling at egress saturation.

--- asl_sdr_hackfest/protocols/network_layer_handler.py
+++ asl_sdr_hackfest/protocols/network_layer_handler.py
@@ -1,8 +1,14 @@
 import threading
 import queue
+import time
 
 from asl_sdr_hackfest.protocols.qos import QoS
-from asl_sdr_hackfest.protocols.network_layer import NetworkLayerReceive, NetworkLayerTransmit
+from asl_sdr_hackfest.protocols.network_layer import NetworkLayerReceive, NetworkLayerTransmit, Frame
+
+
+
+EGRESS_WINDOW_LOW = 8
+EGRESS_WINDOW_HIGH = 12
 
 
 
@@ -45,8 +51,10 @@ class NetworkLayerReceiveHandler(threading.Thread, object):
 
 
 class NetworkLayerTransmitHandler(threading.Thread, object):
-    def __init__(self, output_data_func):
+    def __init__(self, output_data_func, max_frames_per_second = 20):
         self.running = False
+
+        self.xmit_delay = float(1)/float(max_frames_per_second)
 
         self.out_queue = queue.Queue()
         self.in_queues = []
@@ -74,7 +82,43 @@ class NetworkLayerTransmitHandler(threading.Thread, object):
         self.running = True
 
         while (self.running is True):
-            self.tx_class.do_transmit(mtu = 255)
+            self.tx_class.do_transmit(mtu = 96 - Frame.headerlength)
+            time.sleep(self.xmit_delay)
             if self.out_queue.empty() is False:
+                if self.out_queue.qsize() > EGRESS_WINDOW_HIGH:
+                    self.egress_cull()
                 data = self.out_queue.get()
                 self.output_data_func(data)
+
+
+    def egress_cull(self):
+        init_size = self.out_queue.qsize()
+
+        tmp_egress = []
+        while self.out_queue.empty() is False:
+            tmp_egress.append(self.out_queue.get())
+
+        while len(tmp_egress) > EGRESS_WINDOW_LOW:
+            max_cos = 0
+            for pkt in tmp_egress:
+                cos = pkt[Frame.qos_pos] & 0x0f
+                if cos > max_cos:
+                    max_cos = cos
+
+            candidates = []
+            for i in range(0, len(tmp_egress)):
+                cos = tmp_egress[i][Frame.qos_pos] & 0x0f
+                if cos == max_cos:
+                    candidates.append(i)
+
+            candidates.reverse()
+            while len(tmp_egress) > EGRESS_WINDOW_LOW and len(candidates) > 0:
+                target = candidates.pop()
+                del(tmp_egress[target])
+
+        tmp_egress.reverse()
+        while len(tmp_egress) > 0:
+            self.out_queue.put(tmp_egress.pop())
+
+        culled_size = self.out_queue.qsize()
+        print('Egress queue culled from {} to {}.'.format(init_size, culled_size))
