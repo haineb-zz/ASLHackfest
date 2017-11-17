6874263e92c8f850b9ab458010d22b415e68772e
Zachary Kolarich <Zmathue@gmail.com>
2017-11-17 14:21:37 -0500

    added zmq

--- python-iwlist/viz.py
+++ python-iwlist/viz.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+import numpy
+import pmt
+import zmq
+
 import numpy as np
 import time
 
@@ -14,8 +18,24 @@ class Access_Point(object):
         self.sig_level = sig_level
 
 class Viz(object):
-    def __init__(self):
+    def __init__(self, portIn, timeout = 100):
         self.access_points = dict()
+
+        self._zmqContext = zmq.Context()
+        self._socketIn = self._zmqContext.socket(zmq.SUB)
+        self._socketIn.RCVTIMEO = timeout
+        self._socketIn.connect('tcp://%s:%s' % (self._ipAddress,self._portIn))
+        try:
+            self._socketIn.setsockopt(zmq.SUBSCRIBE, '') # python2
+        except TypeError:
+            self._socketIn.setsockopt_string(zmq.SUBSCRIBE, '') # python3, if this throws an exception... give up...
+
+    def recv(self):
+        msg = self._socketIn.recv()
+        pdu = pmt.deserialize_str(msg)
+        cdr = pmt.to_python(pmt.cdr(pdu))
+        cdr = numpy.getbuffer(cdr)
+        return cdr
 
 
     def update_ap(self, ap):
@@ -23,6 +43,6 @@ class Viz(object):
 
 
     def run(self):
-        N = len(access_points)
+        N = len(self.access_points)
         fig, ax = plt.subplots(ncols=11)
         plt.bar()
